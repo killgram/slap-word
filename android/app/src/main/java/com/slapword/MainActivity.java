327b92ceb42e0d548778310c20a970c0f47037a3
killgram <killgram94@gmail.com>
2022-10-25 22:08:14 +0600

    add android splash screen

--- android/app/src/main/java/com/slapword/MainActivity.java
+++ android/app/src/main/java/com/slapword/MainActivity.java
@@ -1,10 +1,18 @@
 package com.slapword;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
